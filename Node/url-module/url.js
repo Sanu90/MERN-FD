15a6da18afa8a3c4ca45f46fc413956af98b7355
Sanup Divakaran <sanukyr@gmail.com>
2024-10-26 00:50:17 +0530

    URL class & parse method

--- Node/url-module/url.js
+++ Node/url-module/url.js
@@ -1,15 +1,40 @@
-// URL parse
+// // URL parse
 
-const http = require("http");
-const url = require("url");
+// const http = require("http");
+// const url = require("url");
 
-const server = http.createServer((req, res) => {
-  const myURL = url.parse(req.url, true);
-  console.log("URL-->", myURL);
-  console.log("myURL.query", myURL.query);
-  console.log("myURL.pathname", myURL.pathname);
+// const server = http.createServer((req, res) => {
+//   console.log("req", req.url, req.upgrade);
 
-  res.end(`Hello, your search results for ${myURL.query.query} is not available now.`);
-});
+//   //   const myURL = url.parse(req.url, true);
+//   //   console.log("URL-->", myURL);
+//   //   console.log("myURL.query", myURL.query);
+//   //   console.log("myURL.pathname", myURL.pathname);
 
-server.listen(1118, () => console.log("Server running"));
+//   res.end(
+//     "Hello"
+//     // `Hello, your search results for ${myURL.query.q} is not available now.`
+//   );
+// });
+
+// server.listen(1118, () => console.log("Server running"));
+
+//using URL class//
+const { URL } = require("url");
+const myURL = new URL("http://example.com/path?product=book&price=20");
+console.log("myURL-->", myURL);
+console.log(myURL.protocol);
+console.log(myURL.host);
+console.log(myURL.pathname);
+console.log(myURL.search);
+console.log(myURL.searchParams.get("product"));
+console.log(myURL.searchParams.get("price"));
+
+// using parse method //
+// const url = require("url");
+// const myURL = url.parse("http://example.com/path?product=book&price=20", true);
+// console.log(myURL.protocol);
+// console.log("\n", myURL.hostname);
+// console.log("\n", myURL.pathname);
+// console.log("\n", myURL.search);
+// console.log("\n", myURL.query.product, "--->", myURL.query.price);
