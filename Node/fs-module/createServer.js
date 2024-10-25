bd031eb715372b125e3a97118f749ae6d324a290
Sanup Divakaran <sanukyr@gmail.com>
2024-10-25 20:42:47 +0530

    some updates

--- Node/fs-module/createServer.js
+++ Node/fs-module/createServer.js
@@ -1,23 +1,71 @@
-// create a server using node.js
+// // create a server using node.js
+
+// const http = require("http");
+// const fs = require("fs");
+
+// const server = http.createServer((req, res) => {
+//   console.log("hey", req.url);
+
+//   let date1 = new Date().toString();
+//   let date2 = Date.now();
+//   if (req.url == "/") {
+//     fs.appendFile("./newFile.txt", `\n ${date1}+ ${date2}`, (err) => {
+//       if (err) {
+//         console.log("Error happened", err);
+//       } else {
+//         res.writeHead(200, { "Content-Type": "text/plain" });
+//         res.write(`${date1} ${date2} is written in log file\n`);
+//         res.end("Ended");
+//         console.log("hey");
+//       }
+//     });
+//   } else {
+//     res.end("Sorry, Log is not maintained for this path.");
+//   }
+// });
+
+// server.listen(1110, () => {
+//   console.log("Server up");
+// });
+
+// const http = require("http");
+
+// const server = http.createServer((req, res) => {
+//   console.log("hey", req.url);
+//   const url = req.url;
+//   console.log(url.length);
+
+//   if ((req, url == "/")) {
+//     res.end("This is home page");
+//   } else {
+//     res.end(`This is ${url} page`);
+//   }
+
+//   //   res.write("Hello\n");
+//   //   res.end("End");
+// });
+
+// server.listen(1990, () => console.log("server @1990"));
 
 const http = require("http");
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  let date1 = new Date().toString();
-  let date2 = Date.now();
-  fs.appendFile("./newFile.txt", `\n ${date1}+ ${date2}`, (err) => {
-    if (err) {
-      console.log("Error happened", err);
-    } else {
-      res.writeHead(200, { "Content-Type": "text/plain" });
-      res.write(`${date1} ${date2} is written in log file\n`);
-      res.end("Ended");
-      console.log("hey");
-    }
-  });
+  console.log(1);
+  if (req.url == "/") {
+    fs.readFile("./newFile.txt", "utf-8", (err, data) => {
+      if (err) {
+        console.log("Error", err);
+      } else {
+        console.log("2, data is-->", data);
+        res.end("Data is read");
+        console.log(4);
+      }
+    });
+  }
+  console.log(3);
 });
 
-server.listen(1110, () => {
+server.listen(2299, () => {
   console.log("Server up");
 });
