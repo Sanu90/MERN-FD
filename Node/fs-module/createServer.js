085d996c6ce5e2e4e0d5d3ca5948aaba1ea9f701
Sanup Divakaran <sanukyr@gmail.com>
2024-10-25 16:15:29 +0530

    node server created + fs module also

--- Node/fs-module/createServer.js
+++ Node/fs-module/createServer.js
@@ -1,12 +1,21 @@
 // create a server using node.js
 
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.write("This is written in browser page");
-  res.end("Ended");
-  console.log("hey");
+  let date1 = new Date().toString();
+  let date2 = Date.now();
+  fs.appendFile("./newFile.txt", `\n ${date1}+ ${date2}`, (err) => {
+    if (err) {
+      console.log("Error happened", err);
+    } else {
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.write(`${date1} ${date2} is written in log file\n`);
+      res.end("Ended");
+      console.log("hey");
+    }
+  });
 });
 
 server.listen(1110, () => {
--- Node/fs-module/node1.js
+++ Node/fs-module/node1.js
@@ -1,10 +1,78 @@
-// const add = require("./arithmetic");
+// // // const add = require("./arithmetic");
 
-// import { add, sub } from "./arithmetic.js";
+// // // import { add, sub } from "./arithmetic.js";
 
-const fs = require("fs");
+// // const fs = require("fs");
 
-// console.log(add(4, 5));
-// console.log(sub(90, 90));
+// // // console.log(add(4, 5));
+// // // console.log(sub(90, 90));
 
-console.log(fs);
+// // console.log(fs);
+
+// const fs = require("fs");
+
+// const employeeData = [
+//   {
+//     name: "srikhar",
+//     id: "E12P",
+//     age: 43,
+//     contact: 9858745108,
+//     Gratuity: true,
+//   },
+//   {
+//     name: "girija",
+//     id: "Z20P",
+//     age: 30,
+//     contact: 9855045108,
+//     Gratuity: false,
+//   },
+//   {
+//     name: "deepak",
+//     id: "J99O",
+//     age: 33,
+//     contact: 9002575108,
+//     Gratuity: false,
+//   },
+//   {
+//     name: "Housad",
+//     id: "MM88",
+//     age: 28,
+//     contact: 9812121108,
+//     Gratuity: false,
+//   },
+// ];
+
+// let ageLessThan30 = employeeData.filter((value) => {
+//   return value.age < 30;
+// });
+// let ageEqualTo30 = employeeData.filter((value) => {
+//   return value.age == 30;
+// });
+// let nameStartsWithD = employeeData.filter((value) => {
+//   return value.name.includes("deepak");
+// });
+
+// //console.log("ageLessThan30-->", ageLessThan30);
+
+// fs.writeFile("./createObj.txt", JSON.stringify(ageLessThan30), (error) => {
+//   if (error) {
+//     console.log("error-->", error);
+//   } else {
+//     console.log("Data written successfully");
+//   }
+// });
+
+// fs.appendFile(
+//   "./createObj.txt",
+//   `\n ${JSON.stringify(ageEqualTo30)}\n ${JSON.stringify(nameStartsWithD)}`,
+//   (error) => {
+//     if (error) {
+//       console.log("error", error);
+//     } else {
+//       console.log("data appended");
+//     }
+//   }
+// );
+
+const os = require("os");
+console.log(os.cpus().length);
