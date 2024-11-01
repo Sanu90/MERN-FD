1cde1f08c6eed0dfd8639f0e2b3d1648a3eb0f0d
Sanup Divakaran <sanukyr@gmail.com>
2024-11-01 22:28:29 +0530

    DSA basics

---
+++ DSA/basic-basics.js
@@ -0,0 +1,25 @@
+// // Fibonacci series--> 0,1,1,2,3,5,.....
+// const n=5;
+// let array=[0,1];
+// function fibo(n){
+//     for(let i=2;i<=n;i++){
+//         array[i]=array[i-1]+array[i-2];
+//     }
+//     return array;
+// }
+
+// console.log(fibo(5))
+
+
+// factorial of a number 5! = 5*4!
+
+// let number=10;
+// function fact(n){
+// if(n<2){
+//     return 1;
+// }
+// return n*fact(n-1);
+// }
+
+// console.log(fact(number));
+
