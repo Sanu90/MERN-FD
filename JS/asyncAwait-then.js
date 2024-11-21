640f436af2188e551f04963b2ef3e97a5cabeca4
Sanup Divakaran <sanukyr@gmail.com>
2024-11-21 18:05:43 +0530

    async/await vs promise.then.catch

---
+++ JS/asyncAwait-then.js
@@ -0,0 +1,50 @@
+// https://jsonplaceholder.typicode.com/users/
+
+// PROMISE.THEN
+
+// const number = 11;
+// const promise = new Promise((resolve, reject) => {
+//   console.log("It started");
+
+//   if (number > 5) {
+//     setTimeout(() => {
+//       resolve("This is resolved");
+//     }, 0);
+//   } else {
+//     reject("This is rejected. Sorry");
+//   }
+//   console.log("It ended");
+// });
+
+// promise
+//   .then((data) => {
+//     console.log("output is->", data);
+//   })
+//   .catch((err) => {
+//     console.log("Error happened->", err);
+//   });
+
+//---------------------------------------------------------------
+
+// ASYNC-AWAIT
+
+async function dataFetch() {
+  try {
+    //throw new Error("Nadakkila");
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/sanup`
+    );
+
+    if (response.status != 200) {
+      throw new Error("Error anu mone");
+    }
+    // console.log("response-->", response);
+
+    const data = await response.json();
+    console.log("data is-->", data);
+  } catch (err) {
+    console.log("error happened", err);
+  }
+}
+
+dataFetch();
