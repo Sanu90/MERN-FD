04378023953b1abd1b058e0ec45dff253d8a2b55
Sanup Divakaran <sanukyr@gmail.com>
2024-12-02 17:53:38 +0530

    hoisting

--- JS/hoisting.js
+++ JS/hoisting.js
@@ -1,7 +1,24 @@
-console.log(a); // undefined bcz of hoisting
-var a = 10;
+// console.log(a); // undefined bcz of hoisting
+// var a = 10;
 
-console.log(b, c); // Reference error as bcz of TDZ
+// console.log(b, c); // Reference error as bcz of TDZ
 
-let b = 9;
-const c = 10;
+// let b = 9;
+// const c = 10;
+
+
+var n=10;
+console.log(first)  // undefined
+console.log(second)  // undefined
+
+function square(num){
+  var ans=num*num;
+  return ans;
+}
+
+var first  = square(n);
+var second = square(5);
+
+console.log(first)  // 100
+console.log(second)  // 25
+
