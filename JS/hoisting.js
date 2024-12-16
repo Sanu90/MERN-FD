fb98de22135169f002a7e6ded5cb6795ceae34db
Sanup Divakaran <sanukyr@gmail.com>
2024-12-16 19:32:50 +0530

    sort array using setTimeout. But cannot be used for very high values

--- JS/hoisting.js
+++ JS/hoisting.js
@@ -6,19 +6,17 @@
 // let b = 9;
 // const c = 10;
 
+var n = 10;
+console.log(first); // undefined
+console.log(second); // undefined
 
-var n=10;
-console.log(first)  // undefined
-console.log(second)  // undefined
-
-function square(num){
-  var ans=num*num;
+function square(num) {
+  var ans = num * num;
   return ans;
 }
 
-var first  = square(n);
+var first = square(n);
 var second = square(5);
 
-console.log(first)  // 100
-console.log(second)  // 25
-
+console.log(first); // 100
+console.log(second); // 25
---
+++ JS/sort-using-setTimeout.js
@@ -0,0 +1,11 @@
+const array = [7, 29, 13, 19, 11, 1, 14];
+
+// const array = [7291319, 111, 14];
+
+function sorting(num) {
+  setTimeout(() => {
+    console.log(num);
+  }, num);
+}
+
+array.forEach((item) => sorting(item));
