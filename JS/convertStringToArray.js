a3e3aeff4508cd2d58fad8c0c57a3298a38b858e
Sanup Divakaran <sanukyr@gmail.com>
2024-12-19 14:55:04 +0530

    Convert a string to array using JS

---
+++ JS/convertStringToArray.js
@@ -0,0 +1,14 @@
+const str = "hello";
+
+// convert using spread operator //
+
+let arr1 = [...str];
+console.log(arr1);
+
+// convert using split method //
+
+console.log(str.split(""));
+
+// using Array.from() method //
+
+
